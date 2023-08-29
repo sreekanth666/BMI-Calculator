3662ab497dd3b4d07a2d4c0e8a4f71defa830e08
Sreekanth K <srekthk@gmail.com>
2023-08-29 21:52:42 +0530

    Added suggestions and validations for input

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calculateBmi() {
     height = document.getElementById("height").value
     weight = document.getElementById("weight").value
+    document.getElementById("suggestionUw").style.display = "none"
+    document.getElementById("suggestionOw").style.display = "none"
+    document.getElementById("downArrow").style.display = "none"
 
     if (height == 0 || weight == 0 || isNaN(height) || isNaN(weight)) {
         alert("Enter your weight and height")
@@ -10,16 +13,26 @@ function calculateBmi() {
         bmiValue.innerHTML = bmi.toFixed(2)
         if (bmi < 18.5) {
             attribute.innerHTML = '<p style="color: red;">Underweight</p>';
+            document.getElementById("suggestionUw").style.display = "block"
+            document.getElementById("downArrow").style.display = "block"
         } else if (bmi >= 18.5 && bmi < 24.9) {
             attribute.innerHTML = '<p style="color: green;">Normal Weight</p>';
         } else if (bmi >= 25 && bmi < 29.9) {
             attribute.innerHTML = '<p style="color: orange;">Overweight</p>';
+            document.getElementById("suggestionOw").style.display = "block"
+            document.getElementById("downArrow").style.display = "block"
         } else if (bmi >= 30 && bmi < 34.9) {
             attribute.innerHTML = '<p style="color: red;">Obese (Class I)</p>';
+            document.getElementById("suggestionOw").style.display = "block"
+            document.getElementById("downArrow").style.display = "block"
         } else if (bmi >= 35 && bmi < 39.9) {
             attribute.innerHTML = '<p style="color: red;">Obese (Class II)</p>';
+            document.getElementById("suggestionOw").style.display = "block"
+            document.getElementById("downArrow").style.display = "block"
         } else if (bmi >= 40) {
             attribute.innerHTML = '<p style="color: red;">Obese (Class III)</p>';
+            document.getElementById("suggestionOw").style.display = "block"
+            document.getElementById("downArrow").style.display = "block"
         } else {
             attribute.innerHTML = "Error in calculation";
         }
@@ -31,4 +44,7 @@ function reset() {
     document.getElementById("weight").value = ""
     bmiValue.innerHTML = "00"
     attribute.innerHTML = '<p>Calculate your BMI now</p>';
+    document.getElementById("suggestionUw").style.display = "none"
+    document.getElementById("suggestionOw").style.display = "none"
+    document.getElementById("downArrow").style.display = "none"
 }
